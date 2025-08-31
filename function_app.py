257e8ab0a1feb1ea36dc8de3e316b462517efdd7
kiltaine <jakub.nebrensky@gmail.com>
2025-08-31 20:18:58 +0200

    save

--- function_app.py
+++ function_app.py
@@ -1,25 +1,35 @@
 import azure.functions as func
-import logging
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+from datetime import datetime
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)  # MUSÍ být na top-level
 
+
+
+@app.function_name(name="GenerateCsv")
 @app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def GenerateCsv(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        account_url = "https://kiltaine.blob.core.windows.net"
+        credential = DefaultAzureCredential()
+        blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
+        container_name = "datove-vystupy"
+        blob_name = "test_data.csv"
+        now = datetime.now()
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+        data = {"data1":["test1", now],
+                "data2":["test2", now],
+                "data3":["test3", now]}
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        df = pd.DataFrame(data)
+        csv_output = df.to_csv()
+        blob_path = blob_service_client.get_blob_client(container=container_name,blob=blob_name)
+        blob_path.upload_blob(csv_output, overwrite=True, encoding='utf-8')
+           
+
+
+        return func.HttpResponse("Soubor nahrán", status_code=200)
+    except Exception as e:
+        return func.HttpResponse(f"Chyba: {e}", status_code=500)
